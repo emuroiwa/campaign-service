faebae9970b46c9b5cadc05bdc9e64a396a2509c
Ernest Muroiwa <ernest.muroiwa@payfast.co.za>
2022-10-15 03:00:10 +0200

    Update CampaignController.php

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -3,9 +3,24 @@
 declare(strict_types=1);
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\CampaignRequest;
+use App\Repository\CampaignRepositoryInterface;
+use Illuminate\Http\Response;
 
 class CampaignController extends Controller
 {
-    //
+    public function __construct(
+        private CampaignRepositoryInterface $campaignRepository,
+    ) {
+    }
+
+    public function create(CampaignRequest $request): Response
+    {
+        try {
+            $result = $this->campaign->save($request->string, $request->length);
+            return response($result, Response::HTTP_CREATED);
+        } catch (\Throwable $th) {
+            return response($th->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+    }
 }
